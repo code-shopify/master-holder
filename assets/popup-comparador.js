9475a5853d2c1b51127e16f6d0e725b0cf8d81cd
Christian Alberto Mancera Popoca <cmancerap@gmail.com>
2025-03-10 12:02:49 -0600

    Update de pop up

--- assets/popup-comparador.js
+++ assets/popup-comparador.js
@@ -5,43 +5,31 @@ document.addEventListener('DOMContentLoaded', function() {
     comparadorBtn.addEventListener('click', function(e) {
       e.preventDefault();
       
-      if (typeof jQuery_T4NT !== 'undefined') {
-        jQuery_T4NT('[data-popup-comparador]').magnificPopup({
-          items: {
-            src: '#PopupComparador',
-            type: 'inline'
-          },
-          mainClass: 't4s-popup-comparador t4s-mfp-move-horizontal',
-          removalDelay: 300,
-          closeOnBgClick: true,
-          closeBtnInside: true,
-          showCloseBtn: true,
-          callbacks: {
-            open: function() {
-              console.log('Popup abierto - inicializando comparador');
-              initComparador();
-            }
+      jQuery('[data-popup-comparador]').magnificPopup({
+        items: {
+          src: '#PopupComparador',
+          type: 'inline'
+        },
+        mainClass: 't4s-popup-comparador',
+        removalDelay: 300,
+        callbacks: {
+          open: function() {
+            console.log('Popup abierto - inicializando comparador');
+            initComparador();
           }
-        }).magnificPopup('open');
-      }
+        }
+      }).magnificPopup('open');
     });
   }
-  
-  function initComparador() {
-    const range = document.querySelector('.t4s-comparador-range');
-    const beforeContainer = document.querySelector('.t4s-before-image-container');
-    
-    if (range && beforeContainer) {
-      beforeContainer.style.width = '50%';
-      
-      range.addEventListener('input', function() {
-        beforeContainer.style.width = this.value + '%';
-      });
+});
 
-      range.addEventListener('touchmove', function(e) {
-        e.preventDefault();
-        beforeContainer.style.width = this.value + '%';
-      });
-    }
+function initComparador() {
+  const range = document.querySelector('.t4s-comparador-range');
+  const beforeContainer = document.querySelector('.t4s-before-image-container');
+  
+  if (range && beforeContainer) {
+    range.addEventListener('input', function() {
+      beforeContainer.style.width = this.value + '%';
+    });
   }
-}); 
+} 
