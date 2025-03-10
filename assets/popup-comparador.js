4d667e0608ffa432f6045256194de3460ef1114c
Christian Alberto Mancera Popoca <cmancerap@gmail.com>
2025-03-10 11:02:53 -0600

    Cambios del pop up de comparador

--- assets/popup-comparador.js
+++ assets/popup-comparador.js
@@ -5,19 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
     comparadorBtn.addEventListener('click', function(e) {
       e.preventDefault();
       
-      jQuery_T4NT.magnificPopupT4s.open({
-        items: {
-          src: '#PopupComparador',
-          type: 'inline'
-        },
-        mainClass: 't4s-popup-comparador',
-        removalDelay: 300,
-        callbacks: {
-          open: function() {
-            initComparador();
+      if (typeof jQuery_T4NT !== 'undefined') {
+        jQuery_T4NT('[data-popup-comparador]').magnificPopup({
+          items: {
+            src: '#PopupComparador',
+            type: 'inline'
+          },
+          mainClass: 't4s-popup-comparador t4s-mfp-move-horizontal',
+          removalDelay: 300,
+          closeOnBgClick: true,
+          closeBtnInside: true,
+          showCloseBtn: true,
+          callbacks: {
+            open: function() {
+              console.log('Popup abierto - inicializando comparador');
+              initComparador();
+            }
           }
-        }
-      });
+        }).magnificPopup('open');
+      }
     });
   }
   
@@ -26,7 +32,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const beforeContainer = document.querySelector('.t4s-before-image-container');
     
     if (range && beforeContainer) {
+      beforeContainer.style.width = '50%';
+      
       range.addEventListener('input', function() {
+        beforeContainer.style.width = this.value + '%';
+      });
+
+      range.addEventListener('touchmove', function(e) {
+        e.preventDefault();
         beforeContainer.style.width = this.value + '%';
       });
     }
